d19603576cb2eaca29d460966602e23576bba2c6
ravishu5 <67092051+ravishu5@users.noreply.github.com>
2024-02-10 16:26:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import About from './components/About';
 import Error from './components/Error';
 import Contact from './components/Contact';
 import RestaurantMenu from './components/RestaurantMenu';
-import Profile from './components/profile';
+// import Profile from './components/profile';
 
 const AppLayout = () =>{
     return (
@@ -35,12 +35,12 @@ const appRouter = createBrowserRouter([
             {
                 path: "/about",
                 element: <About/>,
-                children: ([
-                    {
-                        path: "profile",
-                        element: <Profile/>,
-                    },
-                ])
+                // children: ([
+                //     {
+                //         path: "profile",
+                //         element: <Profile/>,
+                //     },
+                // ])
             },
             {
                 path: "/contact",
